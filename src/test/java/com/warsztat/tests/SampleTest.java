61dfaf9645b55981867d03c5e4a2aa70b911cf2a
tszyborski <tszyborski@sii.pl>
2022-01-04 09:54:55 +0100

    initial

--- src/test/java/com/warsztat/tests/SampleTest.java
+++ src/test/java/com/warsztat/tests/SampleTest.java
@@ -2,10 +2,11 @@ package com.warsztat.tests;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.warsztat.base.PropertySupplier;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO task for students fix code to get rid of warnings
 // TODO implement logging in using Selenide - without and with page objects
@@ -13,7 +14,6 @@ import org.junit.jupiter.api.Test;
 // TODO add inheritance/composition to tests
 // TODO implement setup configuration (base url) using PropertySupplier class
 public class SampleTest {
-    private static final PropertySupplier properties = new PropertySupplier();
 
     @BeforeAll
     public static void setupAllureReports() {
@@ -26,4 +26,6 @@ public class SampleTest {
         Selenide.open("http://automationpractice.com/index.php");
 
     }
+
+
 }
